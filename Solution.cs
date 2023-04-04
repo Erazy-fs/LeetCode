40b26720f24cbf3494639da5e491278329e41c05
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:51:59 +0500

    Solution 1
    Runtime: 141 ms
    Memory Usage: 52.4 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,27 @@
 // 485. Max Consecutive Ones
 public class Solution
 {
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int currentCount = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                currentCount++;
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                }
+            }
+            else
+            {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
 }
