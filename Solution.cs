009a90f66e87b4886fa10ce948724b743a51528d
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:05:05 +0500

    Solution 1
    Runtime: 132 ms
    Memory Usage: 56.8 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,30 @@
 // 412. Fizz Buzz
 public class Solution
 {
+    public IList<string> FizzBuzz(int n)
+    {
+        string[] result = new string[n];
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                result[i - 1] = "FizzBuzz";
+            }
+            else if (i % 3 == 0)
+            {
+                result[i - 1] = "Fizz";
+            }
+            else if (i % 5 == 0)
+            {
+                result[i - 1] = "Buzz";
+            }
+            else
+            {
+                result[i - 1] = Convert.ToString(i);
+            }
+        }
+
+        return result;
+    }
 }
