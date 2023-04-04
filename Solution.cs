4c2ebac984d1ca902e1831e6068d049dfda29a55
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 12:13:43 +0500

    Solution 1
    Runtime: 109 ms
    Memory Usage: 40.6 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,23 @@
 // 1346. Check If N and Its Double Exist
 public class Solution
 {
+    public bool CheckIfExist(int[] arr)
+    {
+        Array.Sort(arr);
+
+        int i = 0;
+
+        while (i < arr.Length && arr[i] * 2 <= arr[arr.Length - 1])
+        {
+            int numIndex = Array.FindIndex(arr, num => num == arr[i] * 2);
+
+            if (numIndex != -1 && numIndex != i)
+            {
+                return true;
+            }
+            i++;
+        }
+
+        return false;
+    }
 }
