7d9ef7bd0ccb521e02ca3577ec9a6d865c584620
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:12:49 +0500

    Solution 1
    Runtime: 28 ms
    Memory Usage: 26.3 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,22 @@
 // 1342. Number of Steps to Reduce a Number to Zero
 public class Solution
 {
+    public int NumberOfSteps(int num)
+    {
+        int count = 0;
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else
+            {
+                num--;
+            }
+            count++;
+        }
+
+        return count;
+    }
 }
