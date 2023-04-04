e13533b6f56db6eb58c4fac88bc92f236a00b712
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 10:52:16 +0500

    Solution 1
    Runtime: 126 ms
    Memory Usage: 43.2 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,12 @@
 // 1480. Running Sum of 1d Array
 public class Solution
 {
+    public int[] RunningSum(int[] nums)
+    {
+        for (int i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
 }
