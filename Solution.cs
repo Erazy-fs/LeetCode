12ec9579134aa971f78dd96a3f6fce79eccd03ac
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 12:02:23 +0500

    Solution 1
    Runtime: 175 ms
    Memory Usage: 60.3 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,10 @@
 // 977. Squares of a Sorted Array
 public class Solution
 {
+    public int[] SortedSquares(int[] nums)
+    {
+        nums = nums.Select(num => num * num).ToArray();
+        Array.Sort(nums);
+        return nums;
+    }
 }
