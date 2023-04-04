1a37adc2d123553d1039a27e4d8c5d10e61d955c
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 12:07:43 +0500

    Solution 1
    Runtime: 139 ms
    Memory Usage: 42.9 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,9 @@
 // 88. Merge Sorted Array
 public class Solution
 {
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        nums2.CopyTo(nums1, m);
+        Array.Sort(nums1);
+    }
 }
