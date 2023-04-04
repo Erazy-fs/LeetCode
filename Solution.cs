d1dbb6e50403911e84b0e1bdbd377ebde11c34e1
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:27:23 +0500

    Solution 2
    Runtime: 96 ms
    Memory Usage: 43.1 MB

--- Solution.cs
+++ Solution.cs
@@ -4,11 +4,38 @@ public class Solution
 {
     public bool CanConstruct(string ransomNote, string magazine)
     {
+        Dictionary<char, int> rnDict = new Dictionary<char, int>();
+        Dictionary<char, int> magDict = new Dictionary<char, int>();
+
         for (int i = 0; i < ransomNote.Length; i++)
         {
-            if (magazine.Contains(ransomNote[i]))
+            if (rnDict.ContainsKey(ransomNote[i]))
             {
-                magazine = magazine.Remove(magazine.IndexOf(ransomNote[i]), 1);
+                rnDict[ransomNote[i]]++;
+            }
+            else
+            {
+                rnDict.Add(ransomNote[i], 1);
+            }
+        }
+
+        for (int i = 0; i < magazine.Length; i++)
+        {
+            if (magDict.ContainsKey(magazine[i]))
+            {
+                magDict[magazine[i]]++;
+            }
+            else
+            {
+                magDict.Add(magazine[i], 1);
+            }
+        }
+
+        foreach (var letter in rnDict)
+        {
+            if (magDict.ContainsKey(letter.Key) && magDict[letter.Key] >= letter.Value)
+            {
+                continue;
             }
             else
             {
