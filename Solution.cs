4d90b2c2b3f25070d700968a117fcab536c35583
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:22:42 +0500

    Solution 4
    Runtime: 86 ms
    Memory Usage: 38 MB

--- Solution.cs
+++ Solution.cs
@@ -5,12 +5,11 @@ public class Solution
     public ListNode MiddleNode(ListNode head)
     {
         List<ListNode> result = new();
-        while (head.next != null)
+        while (head != null)
         {
             result.Add(head);
             head = head.next;
         }
-        result.Add(head);
 
         return result[result.Count / 2];
     }
