d124ac0d5c7fdf477f663657eb92b0ac8ec33891
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 12:16:53 +0500

    Solution 1
    Runtime: 113 ms
    Memory Usage: 48.8 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,37 @@
 // 941. Valid Mountain Array
 public class Solution
 {
+    public bool ValidMountainArray(int[] arr)
+    {
+        int index = 2;
+
+        if (arr.Length < 3 || arr[0] >= arr[1])
+        {
+            return false;
+        }
+
+        while (index < arr.Length - 1)
+        {
+            if (arr[index - 1] == arr[index])
+            {
+                return false;
+            }
+            else if (arr[index - 1] > arr[index])
+            {
+                break;
+            }
+            index++;
+        }
+
+        while (index < arr.Length)
+        {
+            if (arr[index - 1] <= arr[index])
+            {
+                return false;
+            }
+            index++;
+        }
+
+        return true;
+    }
 }
