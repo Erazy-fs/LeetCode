f76e7afa7486b3a7672e241b484442b8ed3528d4
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 10:39:31 +0500

    Solution 1
    Runtime: 85 ms
    Memory Usage: 38.4 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,13 @@
 // 226. Invert Binary Tree
 public class Solution
 {
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+        (root.left, root.right) = (InvertTree(root.right), InvertTree(root.left));
+        return root;
+    }
 }
