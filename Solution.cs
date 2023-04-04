e2ace81fc5ed4e8ed161261d1b5a3a82f03b1d5c
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:21:09 +0500

    Solution 1
    Runtime: 81 ms
    Memory Usage: 38.1 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,20 @@
 // 876. Middle of the Linked List
 public class Solution
 {
+    public ListNode MiddleNode(ListNode head)
+    {
+        byte iter = 1;
+        ListNode result = head;
+        while (head.next != null)
+        {
+            if (iter % 2 != 0)
+            {
+                result = result.next;
+            }
+            head = head.next;
+            iter++;
+        }
+
+        return result;
+    }
 }
