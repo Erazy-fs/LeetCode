776d988b62c91b4b7dfad5748f28835b6f563ed1
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 12:09:20 +0500

    Solution 1
    Runtime: 144 ms
    Memory Usage: 42.5 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,22 @@
 // 27. Remove Element
 public class Solution
 {
+    public int RemoveElement(int[] nums, int val)
+    {
+        int shift = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val)
+            {
+                shift++;
+            }
+            else
+            {
+                nums[i - shift] = nums[i];
+            }
+        }
+
+        return nums.Length - shift;
+    }
 }
