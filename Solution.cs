24e0ea779f9cf897a174879ae41bac81449a2505
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 12:19:22 +0500

    Solution 1
    Runtime: 263 ms
    Memory Usage: 62.6 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,24 @@
 // 1299. Replace Elements with Greatest Element on Right Side
 public class Solution
 {
+    public int[] ReplaceElements(int[] arr)
+    {
+        int borderIndex = 0;
+
+        while (borderIndex < arr.Length - 1)
+        {
+            int maxVal = arr[(borderIndex + 1)..].Max();
+            int newBorderIndex = Array.IndexOf(arr[(borderIndex + 1)..], maxVal) + borderIndex + 1;
+
+            for (int i = borderIndex; i < newBorderIndex; i++)
+            {
+                arr[i] = maxVal;
+            }
+            borderIndex = newBorderIndex;
+        }
+
+        arr[arr.Length - 1] = -1;
+
+        return arr;
+    }
 }
