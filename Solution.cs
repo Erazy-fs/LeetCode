3a6c72e6fffc7be1fae2c103555cb8e9c3118917
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 12:05:40 +0500

    Solution 1
    Runtime: 162 ms
    Memory Usage: 43.9 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,25 @@
 // 1089. Duplicate Zeros
 public class Solution
 {
+    public void DuplicateZeros(int[] arr)
+    {
+        int[] arrCopy = new int[arr.Length];
+        Array.Copy(arr, arrCopy, arr.Length);
+        int shift = 0;
+
+        for (int i = 0; i + shift < arrCopy.Length; i++)
+        {
+            arr[i + shift] = arrCopy[i];
+
+            if (arrCopy[i] == 0)
+            {
+                shift++;
+                if (i + shift == arrCopy.Length)
+                {
+                    break;
+                }
+                arr[i + shift] = arrCopy[i];
+            }
+        }
+    }
 }
