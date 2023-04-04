8bf23478941f6c0a2cedcca228a6dd0c5f4f6591
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:55:45 +0500

    Solution 1
    Runtime: 81 ms
    Memory Usage: 39.5 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,8 @@
 // 1295. Find Numbers with Even Number of Digits
 public class Solution
 {
+    public int FindNumbers(int[] nums)
+    {
+        return nums.Select(x => x.ToString()).Where(x => (x.Length & 1) == 0).Count();
+    }
 }
