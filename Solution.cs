049e920aa11d22d8fe1943b93e1a755546ed6cd9
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 12:11:19 +0500

    Solution 1
    Runtime: 144 ms
    Memory Usage: 45.9 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,22 @@
 // 26. Remove Duplicates from Sorted Array
 public class Solution
 {
+    public int RemoveDuplicates(int[] nums)
+    {
+        int shift = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                shift++;
+            }
+            else
+            {
+                nums[i - shift] = nums[i];
+            }
+        }
+
+        return nums.Length - shift;
+    }
 }
