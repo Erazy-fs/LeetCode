1b813c58cf4fcc659d4a62e73b4232243cd9a202
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:28:40 +0500

    Solution 4
    Runtime: 72 ms
    Memory Usage: 43.3 MB

--- Solution.cs
+++ Solution.cs
@@ -4,28 +4,24 @@ public class Solution
 {
     public bool CanConstruct(string ransomNote, string magazine)
     {
-        Dictionary<char, int> rnDict = new Dictionary<char, int>();
         Dictionary<char, int> magDict = new Dictionary<char, int>();
-
-        for (int i = 0; i < ransomNote.Length; i++)
-        {
-            rnDict[ransomNote[i]] = rnDict.GetValueOrDefault(ransomNote[i], 0) + 1;
-        }
 
         for (int i = 0; i < magazine.Length; i++)
         {
             magDict[magazine[i]] = magDict.GetValueOrDefault(magazine[i], 0) + 1;
         }
 
-        foreach (var letter in rnDict)
+        for (int i = 0; i < ransomNote.Length; i++)
         {
-            if (magDict.ContainsKey(letter.Key) && magDict[letter.Key] >= letter.Value)
+            int count = magDict.GetValueOrDefault(ransomNote[i], 0);
+
+            if (count == 0)
             {
-                continue;
+                return false;
             }
             else
             {
-                return false;
+                magDict[ransomNote[i]] = --count;
             }
         }
 
