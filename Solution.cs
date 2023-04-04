e827c69fdbb1ab798854af2fcc58b4c3047a2677
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-04 11:26:50 +0500

    Solution 1
    Runtime: 115 ms
    Memory Usage: 56.7 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,20 @@
 // 383. Ransom Note
 public class Solution
 {
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        for (int i = 0; i < ransomNote.Length; i++)
+        {
+            if (magazine.Contains(ransomNote[i]))
+            {
+                magazine = magazine.Remove(magazine.IndexOf(ransomNote[i]), 1);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
