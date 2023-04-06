7c46cf951150db4a9182c333bd48c00dbd5261d8
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-06 16:16:28 +0500

    Solution 1
    Runtime: 164 ms
    Memory Usage: 53.9 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,27 @@
 // 283. Move Zeroes
 public class Solution
 {
+    public void MoveZeroes(int[] nums)
+    {
+        int writerIndex = 0;
+        int zerosCount = 0;
+
+        for (int readerIndex = 0; readerIndex < nums.Length; readerIndex++)
+        {
+            if (nums[readerIndex] == 0)
+            {
+                zerosCount++;
+            }
+            else
+            {
+                nums[writerIndex] = nums[readerIndex];
+                writerIndex++;
+            }
+        }
+
+        for (int i = writerIndex; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+    }
 }
