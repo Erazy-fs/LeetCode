4a1d229112c5835adbf73014ad99c1915480f747
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-06 17:09:46 +0500

    Solution 1
    Runtime: 146 ms
    Memory Usage: 46.5 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,24 @@
 // 905. Sort Array By Parity
 public class Solution
 {
+    public int[] SortArrayByParity(int[] nums)
+    {
+        int endPointer = nums.Length - 1;
+
+        for (int i = 0; i < endPointer; i++)
+        {
+            while (nums[endPointer] % 2 != 0 && endPointer > i)
+            {
+                endPointer--;
+            }
+
+            if (nums[i] % 2 != 0)
+            {
+                (nums[i], nums[endPointer]) = (nums[endPointer], nums[i]);
+                endPointer--;
+            }
+        }
+
+        return nums;
+    }
 }
