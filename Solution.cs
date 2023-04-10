bdc1831a8a6b8bc7c06d78c6166e0feaa709471f
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-10 14:06:10 +0500

    Solution 1
    Runtime: 80 ms
    Memory Usage: 38.5 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,22 @@
 // 1051. Height Checker
 public class Solution
 {
+    public int HeightChecker(int[] heights)
+    {
+        int count = 0;
+        int[] heightsSorted = new int[heights.Length];
+
+        heights.CopyTo(heightsSorted, 0);
+        Array.Sort(heightsSorted);
+
+        for (int i = 0; i < heights.Length; i++)
+        {
+            if (heights[i] != heightsSorted[i])
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
