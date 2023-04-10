61a805d7fdaeaa8925797734328b79bed46d42a7
Ilnar_Nukhov <erazy.fs@gmail.com>
2023-04-10 17:30:55 +0500

    Solution 1
    Runtime: 99 ms
    Memory Usage: 39.8 MB

--- Solution.cs
+++ Solution.cs
@@ -2,4 +2,10 @@
 // 414. Third Maximum Number
 public class Solution
 {
+    public int ThirdMax(int[] nums)
+    {
+        Array.Sort(nums);
+        nums = nums.Distinct().ToArray();
+        return nums.Length < 3 ? nums[nums.Length - 1] : nums[nums.Length - 3];
+    }
 }
