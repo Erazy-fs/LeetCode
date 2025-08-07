994c2c16f96b8e2f3e5ffffdbe0def3f67b14a8b
Erazy-fs <102290233+Erazy-fs@users.noreply.github.com>
2025-08-07 23:35:24 +0500

    Update Solution.cs

--- Solution.cs
+++ Solution.cs
@@ -1,9 +1,9 @@
-﻿// Beginner's Guide
+// Beginner's Guide
 // 1672. Richest Customer Wealth
 public class Solution
 {
     public int MaximumWealth(int[][] accounts)
     {
-        return (accounts.Select(x => x.Sum()).Max());
+        return accounts.Select(x => x.Sum()).Max();
     }
 }
